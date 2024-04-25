fd06768cdfcc1f751424f5f0a60991c6e00949b8
Christian <c.delicaat@hotmail.com>
2024-04-25 16:39:30 +0200

    Added namespace to log class

--- engine/source/private/core/log.cpp
+++ engine/source/private/core/log.cpp
@@ -1,11 +1,14 @@
 ﻿#include "core/log.hpp"
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-std::shared_ptr<spdlog::logger> Log::sLogger;
-
-void Log::Init()
+namespace lumina
 {
-    spdlog::set_pattern("%^[%T] %n: %v%$");
-    sLogger = spdlog::stdout_color_mt("LUMINA");
-    sLogger->set_level(spdlog::level::trace);
+    std::shared_ptr<spdlog::logger> Log::sLogger;
+
+    void Log::Init()
+    {
+        spdlog::set_pattern("%^[%T] %n: %v%$");
+        sLogger = spdlog::stdout_color_mt("LUMINA");
+        sLogger->set_level(spdlog::level::trace);
+    }
 }
--- engine/source/public/core/log.hpp
+++ engine/source/public/core/log.hpp
@@ -3,20 +3,23 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 
-class Log
+namespace lumina
 {
-public:
-    static void Init();
+    class Log
+    {
+    public:
+        static void Init();
 
-    inline static std::shared_ptr<spdlog::logger>& GetLogger() { return sLogger; }
+        inline static std::shared_ptr<spdlog::logger>& GetLogger() { return sLogger; }
 
-private:
-    static std::shared_ptr<spdlog::logger> sLogger;
-};
+    private:
+        static std::shared_ptr<spdlog::logger> sLogger;
+    };
+}
 
 // Logging macros
-#define LE_TRACE(...) ::Log::GetLogger()->trace(__VA_ARGS__)
-#define LE_INFO(...)  ::Log::GetLogger()->info(__VA_ARGS__)
-#define LE_WARN(...)  ::Log::GetLogger()->warn(__VA_ARGS__)
-#define LE_ERROR(...) ::Log::GetLogger()->error(__VA_ARGS__)
-#define LE_FATAL(...) ::Log::GetLogger()->fatal(__VA_ARGS__)
+#define LE_TRACE(...) ::lumina::Log::GetLogger()->trace(__VA_ARGS__)
+#define LE_INFO(...)  ::lumina::Log::GetLogger()->info(__VA_ARGS__)
+#define LE_WARN(...)  ::lumina::Log::GetLogger()->warn(__VA_ARGS__)
+#define LE_ERROR(...) ::lumina::Log::GetLogger()->error(__VA_ARGS__)
+#define LE_FATAL(...) ::lumina::Log::GetLogger()->fatal(__VA_ARGS__)
