d2c63d97187746639ff0255105f924367dfb14bd
Vengar van de Wal <vengar_live@hotmail.com>
2024-07-13 23:10:36 +0200

    Added setup for Shaders. And started on Descriptors
    Added a GLSL shader, with a .bat file that converts it to SPIR-V. Added function for Descriptors creation. Added span.hpp which is enables us to use C++20s std::span.

--- engine/source/private/rendering/vk_descriptors.cpp
+++ engine/source/private/rendering/vk_descriptors.cpp
@@ -1,1 +1,74 @@
 ﻿#include "vk_descriptors.hpp"
+
+
+void DescriptorLayoutBuilder::AddBinding(uint32_t binding, VkDescriptorType type)
+{
+    VkDescriptorSetLayoutBinding newBinding {};
+    newBinding.binding            = binding;
+    newBinding.descriptorCount    = 1;
+    newBinding.descriptorType     = type;
+
+    bindings.push_back(newBinding);
+}
+void DescriptorLayoutBuilder::Clear()
+{
+    bindings.clear();
+}
+VkDescriptorSetLayout DescriptorLayoutBuilder::Build(VkDevice device, VkShaderStageFlags shadeStages, void* pNext, VkDescriptorSetLayoutCreateFlags flags)
+{
+    for (auto& binding : bindings)
+    {
+        binding.stageFlags |= shadeStages;
+    }
+
+    VkDescriptorSetLayoutCreateInfo layoutInfo {};
+    layoutInfo.sType        = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
+    layoutInfo.pBindings    = bindings.data();
+    layoutInfo.pNext        = pNext;
+    layoutInfo.flags        = flags;
+
+    VkDescriptorSetLayout setLayout;
+    VK_CHECK(vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr, &setLayout));
+
+    return setLayout;
+}
+void DescriptorAllocator::InitializePool(VkDevice device, uint32_t maxSets, tcb::span<PoolSizeRatio> poolRatios)
+{
+    std::vector<VkDescriptorPoolSize> poolSizes;
+    for (PoolSizeRatio ratio : poolRatios)
+    {
+        
+        poolSizes.push_back(VkDescriptorPoolSize{ratio.type, static_cast<uint32_t>(ratio.ratio) * maxSets});
+    }
+    VkDescriptorPoolCreateInfo poolInfo {};
+    poolInfo.sType         = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
+    poolInfo.pPoolSizes    = poolSizes.data();
+    poolInfo.maxSets       = maxSets;
+    poolInfo.flags         = 0;
+
+    vkCreateDescriptorPool(device, &poolInfo, nullptr, &pool);
+}
+void DescriptorAllocator::ClearDescriptors(VkDevice device)
+{
+    vkResetDescriptorPool(device, pool, 0);
+}
+void DescriptorAllocator::DestroyPool(VkDevice device)
+{
+    vkDestroyDescriptorPool(device, pool, nullptr);
+}
+VkDescriptorSet DescriptorAllocator::Allocate(VkDevice device, VkDescriptorSetLayout layout)
+{
+    VkDescriptorSetAllocateInfo allocInfo {};
+    allocInfo.sType              = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocInfo.descriptorPool     = pool;
+    allocInfo.descriptorSetCount = 1;
+    allocInfo.pSetLayouts        = &layout;
+    allocInfo.pNext              = nullptr;
+
+    VkDescriptorSet descriptorSet;
+    VK_CHECK(vkAllocateDescriptorSets(device, &allocInfo, &descriptorSet));
+
+    return descriptorSet;
+}
--- engine/source/private/rendering/vk_descriptors.hpp
+++ engine/source/private/rendering/vk_descriptors.hpp
@@ -1,3 +1,30 @@
 ﻿#pragma once
 
 #include "vk_types.hpp"
+#include "core/span.hpp"
+
+struct DescriptorLayoutBuilder
+{
+    std::vector<VkDescriptorSetLayoutBinding> bindings {};
+
+    void AddBinding(uint32_t binding, VkDescriptorType type);
+    void Clear();
+    VkDescriptorSetLayout Build(VkDevice device, VkShaderStageFlags shadeStages, void* pNext = nullptr, VkDescriptorSetLayoutCreateFlags flags = 0);
+};
+
+struct DescriptorAllocator
+{
+    struct PoolSizeRatio
+    {
+        VkDescriptorType type;
+        float ratio;
+    };
+
+    VkDescriptorPool pool;
+
+    void InitializePool(VkDevice device, uint32_t maxSets, tcb::span<PoolSizeRatio> poolRatios);
+    void ClearDescriptors(VkDevice device);
+    void DestroyPool(VkDevice device);
+
+    VkDescriptorSet Allocate(VkDevice device, VkDescriptorSetLayout layout);
+};
